9f27e5ddf0a08d6c2ed0ee43c20d3b525a527495
Trucne <91017180+Trucne@users.noreply.github.com>
2021-09-19 20:50:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Mabae à!',
+    introDesc: `Mabae mabae! Làm sao cho anh đến đây và hỏi cưới em mới hay
+    Yeahhh`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnYes: 'Vẫn cứ là thích chị <33',
+    btnNo: 'Không, Chị gái à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu chị <3',
+    btnReply: 'Gửi cho chị yêu <3',
+    reply: 'Vì chị xinhh nhất thế giới  <33333333',
+    mess: 'Chị biết mà 🥰. Yêu pé nhiều nhiều 😘😘',
+    messDesc: 'Hẹn em một năm nữa nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://m.me/HoThanhTruc112k2' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
