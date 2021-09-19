21d13bcdfd1df416f60da530a34ba5e6b6150aa1
Trucne <91017180+Trucne@users.noreply.github.com>
2021-09-19 21:17:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'Mabae à!',
+    introDesc: `Mabae mabae
+    Làm sao cho em đến đây
+    Và hỏi cưới chị mới hay
+    Yeahhhh `,
+    btnIntro: 'iuquaazậy',
+    title: 'Babe em có sợ chị rời xa bỏ hết tình yêu ngày qua 😙',
+    desc: 'Em có sợ khi lời buông bàn tay một mai chị nói ra ',
+    btnYes: 'Sẽ không có đâuu em muốn yêu chị rất lâu <33',
+    btnNo: 'Không, Chị gái à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu chị <3',
+    btnReply: 'Gửi cho chị yêu <3',
+    reply: 'Tại em bị trúng bùa mê gòiiii <33333333',
+    mess: 'Chị biết mà 🥰. Là chị bỏ bùa đó iupé 😘😘',
+    messDesc: 'Hẹn em một năm nữa nha .',
+    btnAccept: 'Dạ vợ iu <3',
+    messLink: 'http://fb.com' / HoThanhTruc112k2 . VD: https://m.me/nam.nodemy
 }
